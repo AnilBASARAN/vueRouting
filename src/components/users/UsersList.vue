3ac89321ed33d7290b1edcb6c5edab6e8c02b9c8
AnilBASARAN <nlbsrn@gmail.com>
2025-06-02 17:42:17 +0300

    for programmatic navigation with $router.... its like navigate with react

--- src/components/users/UsersList.vue
+++ src/components/users/UsersList.vue
@@ -1,6 +1,12 @@
 <template>
+  <button @click="confirmInput">Confirm</button>
   <ul>
-    <user-item v-for="user in users" :key="user.id" :name="user.fullName" :role="user.role"></user-item>
+    <user-item
+      v-for="user in users"
+      :key="user.id"
+      :name="user.fullName"
+      :role="user.role"
+    ></user-item>
   </ul>
 </template>
 
@@ -8,6 +14,11 @@
 import UserItem from './UserItem.vue';
 
 export default {
+  methods: {
+    confirmInput() {
+      this.$router.push('/teams');
+    },
+  },
   components: {
     UserItem,
   },
